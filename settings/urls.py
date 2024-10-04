1f2d5568d89f3c08730a9aa824fd88939142442d
Andrew <skkqw@yandex.ru>
2024-10-04 23:54:57 +0300

    +

--- settings/urls.py
+++ settings/urls.py
@@ -17,7 +17,25 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Documentation",
+        default_version="v1",
+        description="Описание вашего API",
+        terms_of_service="https://www.example.com/terms/",
+        contact=openapi.Contact(email="support@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,5 +44,10 @@ urlpatterns = [
     path('api/products/', include('modules.products.urls')),
     path('api/agents/', include('modules.agents.urls')),
     path('api/contracts/', include('modules.contracts.urls')),
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0),
+          name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

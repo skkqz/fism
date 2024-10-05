49b757ce4a0098c1b5b2108ebbbeb2d6c81ec706
Andrew <skkqw@yandex.ru>
2024-10-05 11:40:10 +0300

    +

---
+++ modules/accounts/migrations/0002_create_super_user.py
@@ -0,0 +1,30 @@
+from django.db import migrations
+from django.contrib.auth import get_user_model
+
+def create_superuser(apps, schema_editor):
+    # Получаем модель пользователя
+    User = get_user_model()
+
+    # Параметры для суперпользователя
+    superuser_email = 'qwe@qwe.com'
+    superuser_username = 'admin'
+    superuser_password = 'qwe'
+
+    # Проверяем, существует ли суперпользователь с таким email
+    if not User.objects.filter(email=superuser_email).exists():
+        # Создаем суперпользователя
+        User.objects.create_superuser(
+            username=superuser_username,
+            email=superuser_email,
+            password=superuser_password
+        )
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounts', '0001_initial'),  # Зависимость от последней миграции auth
+    ]
+
+    operations = [
+        migrations.RunPython(create_superuser),  # Запускаем функцию создания суперпользователя
+    ]
